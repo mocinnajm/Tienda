54f200f23b8a4a46e6d0fee9a6c0859f73c89230
mocinnajm <alge1257@gmail.com>
2023-04-03 23:24:29 +0200

    se agrega el proyecto

---
+++ src/tienda/Tienda.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tienda;
+
+import Vistas.FramLogin;
+
+/**
+ *
+ * @author M
+ */
+public class Tienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FramLogin frm = new FramLogin();
+        frm.setVisible(true);
+        
+    }
+    
+}
